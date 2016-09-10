2046d2f79b2e25246c68be36bceedd886a25b2bb
Conception site Web Premium <info@webpremium.ca>
2016-09-09 22:22:30 -0400

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,6 +2,5 @@
 /// <reference path="globals/cordova/cordova.d.ts" />
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
