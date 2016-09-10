73509bce1eb4331e20f919d46718eb0f362b6a12
Conception site Web Premium <info@webpremium.ca>
2016-09-09 21:04:50 -0400

    1

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/angular-protractor/index.d.ts" />
+/// <reference path="globals/cordova/cordova.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/selenium-webdriver/index.d.ts" />
