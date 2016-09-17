dc82f1157162293b31cb273b7702924d241372e8
Conception site Web Premium <info@webpremium.ca>
2016-09-17 07:32:11 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 var	gulp			= require('gulp'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    tsify = require('tsify'),
+    pretty = require('prettysize'),
+    merge = require('lodash.merge'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    stream = require('stream'),
 	gutil = require('gulp-util'),
 	minifyCss 		= require('gulp-minify-css'),
 	sass			= require('gulp-sass'),
@@ -6,14 +16,12 @@ var	gulp			= require('gulp'),
     runSequence		= require('run-sequence'),
 	gulpif			= require('gulp-if'),
 	rename			= require("gulp-rename"),
-	uglify 			= require('gulp-uglifyjs'),
 	browserSync 	= require('browser-sync').create(),
 	cordova 		= require('gulp-cordova'),
 	isRelease		= false,
 	startFirst		= true,
 	reload 			= browserSync.reload,
 	exec 			= require('child_process').exec,
-	buildBrowserify = require('ionic-gulp-browserify-typescript'),
 	defaultSrc 		= [
 	  'node_modules/es6-shim/es6-shim.min.js',
 	  'node_modules/es6-shim/es6-shim.map',
@@ -25,6 +33,97 @@ var	gulp			= require('gulp'),
 
 
 
+var defaultOptions = {
+  watch: false,
+  src: ['./app/app.ts', './typings/index.d.ts'],
+  outputPath: 'www/js/',
+  outputFile: 'app.bundle.js',
+  minify: false,
+  browserifyOptions: {
+    cache: {},
+    packageCache: {},
+    debug: true
+  },
+  watchifyOptions: {},
+  tsifyOptions: {},
+  uglifyOptions: {},
+  onError: function(err){
+    console.error(err.toString());
+    this.emit('end');
+  },
+  onLog: function(log){
+    console.log((log = log.split(' '), log[0] = pretty(log[0]), log.join(' ')));
+  }
+}
+
+buildBrowserify = function(options) {
+  options = merge(defaultOptions, options);
+
+  var b = browserify(options.src, options.browserifyOptions)
+    .plugin(tsify, options.tsifyOptions);
+
+  if (options.watch) {
+    b = watchify(b, options.watchifyOptions);
+    b.on('update', bundle);
+    b.on('log', options.onLog);
+  }
+
+  return bundle();
+
+  function bundle() {
+    var debug = options.browserifyOptions.debug;
+    return b.bundle()
+      .on('error', options.onError)
+      .pipe(source(options.outputFile))
+      .pipe(buffer())
+      .pipe(debug ? sourcemaps.init({ loadMaps: true }) : noop())
+      .pipe(options.minify ? uglify(options.uglifyOptions) : noop())
+      .pipe(debug ? sourcemaps.write('./',{includeContent:true, sourceRoot:'./'}) : noop())
+      .pipe(gulp.dest(options.outputPath));
+  }
+
+  function noop(){
+    return new stream.PassThrough({ objectMode: true });
+  }
+}
+
+
+
+
+gulp.task('cordova', function() {
+  gulp.src('./package.json')
+    .pipe(cordova())
+});
+
+gulp.task('androidDevice', function() {
+  gulp.src('./configs/androidDevice.json')
+    .pipe(cordova())
+});
+
+gulp.task('androidBuild', function() {
+  gulp.src('./configs/androidBuild.json')
+    .pipe(cordova())
+});
+
+
+function CordovaAndroidBuild(){
+	runSequence(
+    'androidBuild',
+    function (error) {
+      
+      if(typeof callback == "function")callback(error);
+	  
+    });
+}
+function CordovaRunAndroidDevice(){
+	runSequence(
+    'androidDevice',
+    function (error) {
+      
+      if(typeof callback == "function")callback(error);
+	  
+    });
+}
 function CordovaRun(){
 	runSequence(
     'cordova',
@@ -166,10 +265,6 @@ gulp.task('copy-css2', function(options) {
 });
 
 
-gulp.task('cordova', function() {
-  gulp.src('./package.json')
-    .pipe(cordova())
-})
 
 
 gulpcssAdapt = function(options) {
@@ -214,22 +309,36 @@ gulp.task('webserver',['build2', 'build-js2', 'build-js3', 'build-fonts2','copy-
 	CordovaRun();
 	
 	
-	browserSync.init({
-        server: {
-            baseDir: "./platforms/browser/www"
-        }
-    });
+	if( gutil.env.type === 'androidDevice' ){
+		CordovaRunAndroidDevice();
+		
+	}
+	if( gutil.env.type === 'androidBuild' ){
+		CordovaAndroidBuild();
+		
+	}
 	
-	
-	startFirst=false;
-	
-	gulp.watch('app/**/*.scss', ['build-css2']);
-	gulp.watch("app/**/*.css", ['copy-css2']);
-	gulp.watch("app/**/*.+(ttf|eot|ijmap|svg|woff|woff2)", ['build-fonts2']);
-	gulp.watch("app/**/*.js", ['build-js3']);
-	gulp.watch("app/**/*.html", ['build-html2']);
-	gulp.watch("app/**/*.ts",['build2']);
-	
+	if(!gutil.env.type ){
+		
+		browserSync.init({
+			server: {
+				baseDir: "./platforms/browser/www"
+			}
+		});
+		
+		
+		startFirst=false;
+		
+		gulp.watch('app/**/*.scss', ['build-css2']);
+		gulp.watch("app/**/*.css", ['copy-css2']);
+		gulp.watch("app/**/*.+(ttf|eot|ijmap|svg|woff|woff2)", ['build-fonts2']);
+		gulp.watch("app/**/*.js", ['build-js3']);
+		gulp.watch("app/**/*.html", ['build-html2']);
+		gulp.watch("app/**/*.ts",['build2']);
+		
+		
+		
+	}
 
 	
 });
